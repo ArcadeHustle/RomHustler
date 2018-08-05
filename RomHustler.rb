6abcdbdaaf99c8bdada905a0d33aebe326642440
Arcade Hustle <arcadehustle@icloud.com>
2018-08-05 01:47:49 -0400

    Remove old file handler and header dumper for now. Add framework for killing stuck netboot processes. Add manual entry framework

--- RomHustler.rb
+++ RomHustler.rb
@@ -112,6 +112,12 @@ def dissect(rom)
 	return htmldata + "<br> --------------------------------------------------------------------------------------------------- <br><br>"
 end
 
+class ROMKILLER < WEBrick::HTTPServlet::AbstractServlet
+ def do_GET(req, res)
+	p req
+ end
+end
+
 class ROMRUNNER < WEBrick::HTTPServlet::AbstractServlet
  def do_GET(req, res)
 	# /execute?RomBin=AtomisWave%2FAW-GuiltyGearIsuka.bin&NetDimm=192.168.1.95-00%3Ad0%3Af1%3A01%3Ade%3A56
@@ -119,18 +125,17 @@ class ROMRUNNER < WEBrick::HTTPServlet::AbstractServlet
 	# ["/execute?RomBin", "AtomisWave/AW-GuiltyGearIsuka.bin&NetDimm", "192.168.1.191-00:d0:f1:02:1e:4e"]
 
 	rompath = "RomBINS/" + cleanurl[1].split("&")[0] 
-	p rompath
         ip = cleanurl[2].split("-")[0] 
 
 	if File.file?(rompath)
 		puts "Rom file present... "
 		parent_pid = Process.spawn("./netboot_upload_tool", "#{ip}", "#{rompath}")
-		html = "<html><body>Naomi1<br>" + "<a href='../../../'>..</a><br>"
+		html = "<html><body>" + "<a href='../../../'>..</a><br>"
 		html += "./#{rompath} launched with pid: #{parent_pid}"
 	        res.body = html
 	        res['Content-Type'] = "text/html"
 	else
-		html = "<html><body>Naomi1<br>" + "<a href='../../../'>..</a><br>"
+		html = "<html><body>" + "<a href='../../../'>..</a><br>"
 		html += "No rom file!</html>"
         	res.body = html
         	res['Content-Type'] = "text/html"
@@ -146,84 +151,7 @@ end
 class ROMFILEZ < WEBrick::HTTPServlet::FileHandler
  def do_GET(req, res)
         p "Req #{req.unparsed_uri}"
-	Dir.chdir("RomBINS") do
-		rombinfiles = Dir.glob("*").sort_by(&:downcase)
-	end
-
-        if req.unparsed_uri == "/roms/Naomi1/"
-                html = "<html><body>Naomi1<br>" + "<a href='../../'>..</a><br>"
-		Dir.chdir("RomBINS/Naomi1") do
-			naomi1files = Dir.glob("*").sort_by(&:downcase)
-			naomi1files.each{|rom|
-				p "Dissecting #{rom}"
-				html += "<a href='" + req.unparsed_uri + rom + "'>Download: " + rom + "</a><br>"
-				html += "<a href='" + "/execute" + req.unparsed_uri + rom + "'>Execute: " + rom + "</a><br>"
-				html += dissect(rom)				
-			}
-		end
-                html += "</body></html>"
-                res.body = html
-                res['Content-Type'] = "text/html"
-
-        elsif req.unparsed_uri == "/roms/Naomi2/"
-                html = "<html><body>Naomi2<br>" + "<a href='../../'>..</a><br>"
-		Dir.chdir("RomBINS/Naomi2") do
-			naomi2files = Dir.glob("*").sort_by(&:downcase)
-			naomi2files.each{|rom|
-				html += "<a href='" + req.unparsed_uri + rom + "'>Download: " + rom + "</a><br>"
-				html += "<a href='" + "/execute" + req.unparsed_uri + rom + "'>Execute: " + rom + "</a><br>"
-				html += dissect(rom)
-                        }
-		end
-                html += "</body></html>"
-                res.body = html
-                res['Content-Type'] = "text/html"
-
-        elsif req.unparsed_uri == "/roms/AtomisWave/"
-                html = "<html><body>AtomisWave<br>" + "<a href='../../'>..</a><br>"
-		Dir.chdir("RomBINS/AtomisWave") do
-			atomiswavefiles = Dir.glob("*").sort_by(&:downcase)
-			atomiswavefiles.each{|rom|
-				html += "<a href='" + req.unparsed_uri + rom + "'>Download: " + rom + "</a><br>"
-				html += "<a href='" + "/execute" + req.unparsed_uri + rom + "'>Execute: " + rom + "</a><br>"
-				html += dissect(rom)
-                        }
-		end
-                html += "</body></html>"
-                res.body = html
-                res['Content-Type'] = "text/html"
-
-        elsif req.unparsed_uri == "/roms/Chihiro/"
-                html = "<html><body>Chihiro<br>Currently unable to parse Chihiro roms to display more data<br>" + "<a href='../../'>..</a><br>"
-		Dir.chdir("RomBINS/Chihiro") do
-			chihirofiles = Dir.glob("*").sort_by(&:downcase)
-			chihirofiles.each{|rom|
-				html += "<a href='" + req.unparsed_uri + rom + "'>Download: " + rom + "</a><br>"
-				html += "<a href='" + "/execute" + req.unparsed_uri + rom + "'>Execute: " + rom + "</a><br>"
-                        }
-		end
-                html += "</body></html>"
-                res.body = html
-                res['Content-Type'] = "text/html"
-
-        elsif req.unparsed_uri == "/roms/Firmware/"
-                html = "<html><body>Firmware<br>" + "<a href='../../'>..</a><br>"
-		Dir.chdir("RomBINS/Firmware") do
-			firmwarefiles = Dir.glob("*").sort_by(&:downcase)
-			firmwarefiles.each{|rom|
-				html += "<a href='" + req.unparsed_uri + rom + "'>Download: " + rom + "</a><br>"
-				html += "<a href='" + "/execute" + req.unparsed_uri + rom + "'>Execute: " + rom + "</a><br>"
-				html += dissect(rom)
-                        }
-		end
-                html += "</body></html>"
-                res.body = html
-                res['Content-Type'] = "text/html"
-
-        else       
-                print "Calling Super"
-                super
-        end
+        super
  end
 end
 
@@ -231,8 +159,9 @@ class ROMS < HTTPServlet::AbstractServlet
  def do_GET(req, res)
         if req.unparsed_uri == "/"
 		html = "<html><body>"
-		html += "<br>Select the ROM type<br>"
+		html += "<br>Select the Platform type and game ROM<br>"
 		html += "<form action=\"/execute\" method=\"get\"><select name='RomBin'>"
+		html += "<option value='blank'></option>"
                 Dir.chdir("RomBINS/") do
                         files = Dir.glob("*/*").sort_by(&:downcase)
                         files.each{|rom|
@@ -241,14 +170,31 @@ class ROMS < HTTPServlet::AbstractServlet
                 end
 		html += "</select>"
 
-		html += "<br>Select a NetDIMM based DHCP host<br>"
+		html += "<br>Select a NetDIMM based DHCP host or choose manual and enter it yourself<br>"
 		html += "<select name='NetDimm'>"
+		html += "<option value='blank'></option>"
+		html += "<option value='manual'>Manual Entry</option>"
 		get_dhcp_hosts().each{ |host|
 			html += "<option value='#{host[1]}-#{host[0]}'>#{host[1]}</option>"
 		}
 
-		html += "</select><input type=\"submit\" value=\"Submit\"></form>"
-		html += "<br>" + get_running_netboots().to_s + "<br>"
+		# any way to show this only on option select Manual? 
+		html += "</select><br><br>Hostname, or IP for Manual entry:<br>"
+		html += "<input type=\"text\" name=\"host\"><br>"
+		html += "Port (default is 10703):<br>"
+		html += "<input type=\"text\" name=\"port\" value=\"10703\">"		
+
+		html += "<br><br>Click Launch to start the game<br>"
+		html += "<input type=\"submit\" value=\"Launch\"></form>"
+
+		html += "Alternately, Select a running game to kill<br>"
+		html += "<form action=\"/kill\" method=\"get\"><select name='pidtokill'>"
+		get_running_netboots().each{ |pid|
+			p pid
+			html += "<option value=\"#{pid}\">#{pid}</option>"
+		}
+		html += "</select>"
+		html += "<input type=\"submit\" value=\"Kill\"></form>"
 		html += "</body></html>"
         	res.body = html
         	res['Content-Type'] = "text/html"
@@ -265,6 +211,7 @@ end
 
 trap("INT"){ s.shutdown }
 s.mount("/execute", ROMRUNNER)
+s.mount("/kill", ROMKILLER)
 s.mount("/roms", ROMFILEZ, "RomBINS")
 s.mount("/", ROMS)
 s.start
